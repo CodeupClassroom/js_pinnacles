018c148529106e81ff3a799044020245798a7339
Zach Gulde <zachgulde@gmail.com>
2017-04-13 10:38:34 -0500

    add bonus arrays exercise

--- js/arrays.js
+++ js/arrays.js
@@ -36,6 +36,38 @@ var namesString = namesArray.join(' ---- ');
 console.log(namesString);
 
 
+// BONUS:
+// *if* you are finished with the exercise and have a green cup,
+//
+// Create a function to convert from 24 hour to 12 hour time
+// this function takes a string that is a time in the 24 hour
+// format, and returns a string formatted in the 12 hour format
+//
+// Example
+//
+// twentyFourToTwelve("23:00") => "11:00pm"
+// twentyFourToTwelve("06:30") => "6:30am"
+// twentyFourToTwelve("00:00") => "12:00am"
+// twentyFourToTwelve("12:00") => "12:00pm"
+
+// now do it in reverse
+
+// twelveToTwentyFour("11:00pm") => "23:00"
+// twelveToTwentyFour("6:30am") => "06:30"
+// twelveToTwentyFour("12:00am") => "00:00"
+// twelveToTwentyFour("12:00pm") => "12:00"
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
