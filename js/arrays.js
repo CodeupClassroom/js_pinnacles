546a47b9eeb31e32cc83c2b73a7fea52fbaa4c78
Zach Gulde <zachgulde@gmail.com>
2017-04-13 09:44:39 -0500

    add arrays lesson
    this file has headings for all of the arrays subtopics,
    but this commit only adds content for the split-join
    lesson

---
+++ js/arrays.js
@@ -0,0 +1,60 @@
+'use strict';
+
+// Arrays!
+// =======
+// - list of items
+
+// Manipulating Arrays
+// ===================
+// - push + pop
+// - shift + unshift
+// - splice
+// - indexOf
+// - sort + reverse
+
+// Split Join
+// ==========
+// - array <-> string conversion
+
+// var namesString = "Joe,Bob,Sally";
+//
+// console.log(namesString);
+// // Joe,Bob,Sally
+//
+// var namesArray = namesString.split('codeup');
+//
+// console.log(namesArray);
+// // ['Joe', 'Bob', 'Sally']
+
+var namesArray = ['Joe', 'Bob', 'Sally'];
+
+console.log(namesArray);
+// ['Joe', 'Bob', 'Sally']
+
+var namesString = namesArray.join(' ---- ');
+
+console.log(namesString);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
