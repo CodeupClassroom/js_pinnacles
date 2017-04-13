995c8d975eeda1213c664794129ea6993e67a2bb
Zach Gulde <zachgulde@gmail.com>
2017-04-13 13:59:24 -0500

    add objects lesson notes

--- js/objects.js
+++ js/objects.js
@@ -1,13 +1,66 @@
 "use strict";
 
-// 1. wrap this up in an iife, comment out
 // 2. what are objects?
 //    - key - value pairs
 // 3. object literal
-//    - make a pizza
-// 4. accessing object properties, getting and setting
+
+// curly braces, {}, are used to create an object
+// var pizza = {
+//     price: 9.99,
+//     isDelivery: true,
+//     toppings: ['pepperoni', 'green peppers', 'mushrooms', 'olives'],
+//     getPrice: function() {
+//         if (this.isDelivery) {
+//             return this.price + 4.99;
+//         }
+//         return this.price;
+//     }
+// };
+
+// creating the same object as above, but with different syntax
+// start out with an empty object
+// console.log("The price of the pizza is: " + pizza.price);
+
+// 4. add a getPrice method
+
+// 5. accessing object properties, getting and setting
 //    - . notation
 //    - [notation]
-// 5. arrays of objects
+// 6. arrays of objects
 //    - more pizzas
-//    - loop + manipulate
+
+var pizzas = [
+    {
+        price: 9.99,
+        isDelivery: true,
+        toppings: ['pepperoni', 'green peppers', 'mushrooms', 'olives']
+    }, {
+        price: 8.99,
+        isDelivery: true,
+        toppings: ['pepperoni', 'bacon']
+    }, {
+        price: 12.99,
+        isDelivery: false,
+        toppings: ['olives']
+    }, {
+        price: 13.99,
+        isDelivery: false,
+        toppings: ['pineapple', 'ham']
+    }, {
+        price: 9.99,
+        isDelivery: true,
+        toppings: ['extra cheese']
+    }
+];
+
+// display pizzas for delivery
+pizzas.forEach(function(pizza){
+    if (! pizza.isDelivery) {
+        return;
+    }
+    console.log('---------------------------------------------------');
+    console.log("Price: " + pizza.price);
+    console.log("Toppings: " + pizza.toppings.join(", "));
+});
+
+
