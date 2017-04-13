1c6cad5cc83cb52c3805338bcaef340b126ffe67
Zach Gulde <zachgulde@gmail.com>
2017-04-13 10:39:34 -0500

    add lecture outline for objects

---
+++ js/objects.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// 1. wrap this up in an iife, comment out
+// 2. what are objects?
+//    - key - value pairs
+// 3. object literal
+//    - make a pizza
+// 4. accessing object properties, getting and setting
+//    - . notation
+//    - [notation]
+// 5. arrays of objects
+//    - more pizzas
+//    - loop + manipulate
